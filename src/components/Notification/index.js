806f5a69587ac1e1a5ae881eb770999daa8c53fc
Thota-bharat999 <bharatmanithota@gmail.com>
2023-09-01 07:43:59 +0000

    AlertNotification

---
+++ src/components/Notification/index.js
@@ -0,0 +1,15 @@
+import {GrFormClose} from 'react-icons/gr'
+
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+  return (
+    <div className="notification-container">
+      <div className="alert-container">{children}</div>
+      <GrFormClose className="cross" />
+    </div>
+  )
+}
+
+export default Notification
